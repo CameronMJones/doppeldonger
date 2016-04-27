c73f68bf0e7dcb8bfa95378ae235552c9effd842
Cameron Jones <cameron.jones@bluemedora.com>
2016-04-26 22:13:39 -0400

    base 2

--- web.rb
+++ web.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do
   "Hello"
 end
+
+get '/hello/' do
+  erb :hello_form
+end
+
+post '/hello/' do
+  apikey = params[:apikey] || "Hi There"
+
+  erb :index, :locals => {'apikey' => apikey,}
+end
