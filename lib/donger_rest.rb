f36d3444decdf20863feeeb9aa098fb3a843e2f5
Josh Williams <josh.williams@bluemedora.com>
2016-05-04 23:58:25 -0400

    Updated the summoner populate script to use leagues to discover multiple summoners.

--- lib/donger_rest.rb
+++ lib/donger_rest.rb
@@ -3,13 +3,13 @@ require 'json'
 
 class DongerRest
 	KEY = ENV["API_KEY"]
-	LIMIT = ENV["API_RATE_LIMIT"]
+	LIMIT = ENV["API_RATE_LIMIT"].to_f
 
 	def self.get_champions()
 		call("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=tags&api_key=#{KEY}")
 	end
 
-	def self.get_summoner_ids(names)
+	def self.get_summoners(names)
 		sanitized_names = names.map{|name| sanitize(name)}
 		delimited_names = sanitized_names.join(',')
 		call("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/#{delimited_names}?api_key=#{KEY}")
