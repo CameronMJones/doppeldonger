1acd2aa4787cc9574a4e8aab047843f80b23488f
Josh Williams <josh.williams@bluemedora.com>
2016-05-09 13:17:25 -0400

    Fixed a few bugs in the champion startup script.

--- scripts/champion_startup.rb
+++ scripts/champion_startup.rb
@@ -2,31 +2,14 @@ require './lib/donger_rest'
 require './lib/donger_graph'
 require './lib/donger_constants'
 
-begin
-	results = DongerRest.get_champion_info()
-	champions = results[data]
-	champions.each |champion, data| do
-		add_champion(data)
-		add_defense(data)
-		add_difficulty(data)
-		add_damage(data)
-		add_role(data)
-		add_abilities(data)
-	end
-rescue => ex
-	puts "Startup Failed: #{ex.message}"
-end
-
-def add_champion(data)
-	graph = DongerGraph.new()
+def add_champion(data, graph)
 	id = data["id"]
 	name = data["name"]
 	title = data["title"]
 	graph.add_champion(id, name, title)
 end
 
-def add_defense(data)
-	graph = DongerGraph.new()
+def add_defense(data, graph)
 	id = data["id"]
 	defense = data["info"]["defense"]
 	if defense >= 7
@@ -38,10 +21,9 @@ def add_defense(data)
 	end
 end
 
-def add_difficulty(data)
-	graph = DongerGraph.new()
+def add_difficulty(data, graph)
 	id = data["id"]
-	difficulty = stats["info"]["difficulty"]
+	difficulty = data["info"]["difficulty"]
 	if difficulty >= 7
 		graph.add_exhibits(id, DongerConstants::HIGH_DIFFICULTY)
 	elsif difficulty <= 3
@@ -51,11 +33,10 @@ def add_difficulty(data)
 	end
 end
 
-def add_damage(data)
-	graph = DongerGraph.new()
-	id = stats["id"]
-	magic = stats["info"]["magic"]
-	attack = stats["info"]["attack"]
+def add_damage(data, graph)
+	id = data["id"]
+	magic = data["info"]["magic"]
+	attack = data["info"]["attack"]
 	if (magic - attack).abs <= 2
 		graph.add_exhibits(id, DongerConstants::HYBRID_DAMAGE)
 	elsif magic > attack
@@ -65,21 +46,19 @@ def add_damage(data)
 	end
 end
 
-def add_role(data)
-	graph = DongerGraph.new()
+def add_role(data, graph)
 	id = data["id"]
 	data["tags"].each{|role| graph.add_exhibits(id, role)}
 end
 
-def add_abilities(data)
-	graph = DongerGraph.new()
+def add_abilities(data, graph)
 	id = data["id"]
 	labels = data["spells"].map{|spell| spell["leveltip"]["label"]}.flatten.join("|").downcase
 	descriptions = data["spells"].map{|spell| spell["description"]}.flatten.join("|").downcase
 	keywords = labels + "|" + descriptions
 	graph.add_exhibits(id, DongerConstants::SLOW) if keywords.include? "slow"
 	graph.add_exhibits(id, DongerConstants::STUN) if keywords.include? "stun"
-	graph.add_exhibits(id, DongerConstants::ROOT) if keywords.include? "root"
+	graph.add_exhibits(id, DongerConstants::ROOT) if is_root(keywords)
 	graph.add_exhibits(id, DongerConstants::SHIELD) if keywords.include? "shield"
 	graph.add_exhibits(id, DongerConstants::HEAL) if is_heal(keywords)
 	graph.add_exhibits(id, DongerConstants::MOVEMENT) if keywords.include? "movement"
@@ -92,22 +71,47 @@ def add_abilities(data)
 	graph.add_exhibits(id, DongerConstants::DISPLACE) if is_displace(keywords)
 	graph.add_exhibits(id, DongerConstants::MARK) if keywords.include? "mark"
 	graph.add_exhibits(id, DongerConstants::POISON) if keywords.include? "poison"
-	graph.add_exhibits(id, DongerConstants::ZONE) if keywords.include? "zone"
+	graph.add_exhibits(id, DongerConstants::ZONE) if is_zone(keywords)
 	graph.add_exhibits(id, DongerConstants::FIRES) if keywords.include? "fires"
 	graph.add_exhibits(id, DongerConstants::STACKS) if keywords.include? "stack"
 	graph.add_exhibits(id, DongerConstants::MAGIC_RESIST) if keywords.include? "magic resist"
-	graph.add_exhibits(id, DongerConstants::MAGIC_RESIST) if keywords.include? "summons"
+	graph.add_exhibits(id, DongerConstants::SUMMONS) if keywords.include? "summons"
 	graph.add_exhibits(id, DongerConstants::TRUE_DAMAGE) if keywords.include? "true damage"
 end
 
-def is_jump
+def is_root(keywords)
+	return keywords.include?("root")||keywords.include?("binds")
+end
+
+def is_jump(keywords)
 	return keywords.include?("leap")||keywords.include?("teleport")||keywords.include?("dash")
 end
 
 def is_heal(keywords)
-	return keywords.include?("heal")||keywords.include?("restor")||keywords.include?("regen")
+	return keywords.include?("heal|")||keywords.include?("health restor")||keywords.include?("health regen")
 end
 
 def is_displace(keywords)
 	return keywords.include?("grab")||keywords.include?("knock")
+end
+
+def is_zone(keywords)
+	return keywords.include?("zone")||keywords.include?("area")
+end
+
+begin
+	graph = DongerGraph.new()
+	results = DongerRest.get_champion_info()
+	champions = results["data"]
+	champions.each do |champion, data|
+		puts "Adding Champion: #{champion}"
+		add_champion(data, graph)
+		add_defense(data, graph)
+		add_difficulty(data, graph)
+		add_damage(data, graph)
+		add_role(data, graph)
+		add_abilities(data, graph)
+	end
+rescue => ex
+	puts "Startup Failed: #{ex.message}"
 end
